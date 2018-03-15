02f33e1aa099c80dd94545715c4851097293c463
Joanne Song <joannesong@ac.c4q.nyc>
2018-03-15 11:24:02 -0400

    Added a TabLayout and ViewPager for Teacher's Main Page. This contains three fragments: Roster, Progress and Questions

--- app/src/main/java/com/example/franciscoandrade/bloxsee/views/SignInActivity.java
+++ app/src/main/java/com/example/franciscoandrade/bloxsee/views/SignInActivity.java
@@ -26,8 +26,8 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        Intent intent = new Intent(this, TeacherMainPageActivity.class);
-        startActivity(intent);
+//        Intent intent = new Intent(this, TeacherMainPageActivity.class);
+//        startActivity(intent);
 
         setUpViews();
 
