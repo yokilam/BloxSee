109d86ba3258e44d80017e8c6e4661e180584150
7sco <xaviandrade14@gmail.com>
2018-03-17 16:29:32 -0400

    login for kids working need to add blcky

--- app/src/main/java/com/example/franciscoandrade/bloxsee/views/teacher/TeacherMainPageActivity.java
+++ app/src/main/java/com/example/franciscoandrade/bloxsee/views/teacher/TeacherMainPageActivity.java
@@ -1,5 +1,6 @@
 package com.example.franciscoandrade.bloxsee.views.teacher;
 
+import android.graphics.PorterDuff;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
 import com.example.franciscoandrade.bloxsee.R;
@@ -71,5 +73,6 @@ public class TeacherMainPageActivity extends AppCompatActivity {
         return manager.beginTransaction().replace(R.id.fragment_container_teacher, fragment).commit();
 
     }
+
 
 }
