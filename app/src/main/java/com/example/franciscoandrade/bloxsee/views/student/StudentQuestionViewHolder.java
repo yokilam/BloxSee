9b37616eb03286839814733e2b106af8809c9bbd
kevin toro <kevintoro@ac.c4q.nyc>
2018-03-19 18:12:24 -0400

    intent for question itemviews in student activity

--- app/src/main/java/com/example/franciscoandrade/bloxsee/views/student/StudentQuestionViewHolder.java
+++ app/src/main/java/com/example/franciscoandrade/bloxsee/views/student/StudentQuestionViewHolder.java
@@ -1,5 +1,7 @@
 package com.example.franciscoandrade.bloxsee.views.student;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
@@ -7,6 +9,9 @@ import android.widget.TextView;
 
 import com.example.franciscoandrade.bloxsee.R;
 import com.example.franciscoandrade.bloxsee.model.Questions;
+import com.example.franciscoandrade.bloxsee.views.StudentActivity;
+
+import static android.support.v4.content.ContextCompat.startActivity;
 
 /**
  * Created by Kevintoro on 3/18/18.
@@ -15,12 +20,23 @@ import com.example.franciscoandrade.bloxsee.model.Questions;
 public class StudentQuestionViewHolder extends RecyclerView.ViewHolder {
     private TextView studentQuestion;
 
-    public StudentQuestionViewHolder(View itemView) {
+
+
+
+    public StudentQuestionViewHolder(final View itemView) {
         super(itemView);
         studentQuestion = itemView.findViewById(R.id.questions_tv);
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), BlocklyActivity.class);
+                view.getContext().startActivity(intent);
+            }
+        });
     }
     public void onBind(Questions questions){
         studentQuestion.setText(questions.getQuestions());
+
 
     }
 }
