144ca468ddba3846b211d2ac359b41574665bca3
7sco <xaviandrade14@gmail.com>
2018-03-21 18:51:14 -0400

    questions get updated in realtime and checkboxed also get marked

--- app/src/main/java/com/example/franciscoandrade/bloxsee/views/teacher/QuestionsFragment.java
+++ app/src/main/java/com/example/franciscoandrade/bloxsee/views/teacher/QuestionsFragment.java
@@ -38,22 +38,13 @@ public class QuestionsFragment extends Fragment implements View.OnClickListener{
     List<String> listStudents;
     Student studentModel;
     StudentQuestions studentQuestions;
-    HashMap<String, String> lista;
     CheckBox questionOne, questionTwo, questionThree, questionFour, questionFive;
-    StudentQuestions studentQuestions1;
-    StudentQuestions studentQuestions2;
-    StudentQuestions studentQuestions3;
-    StudentQuestions studentQuestions4;
-    StudentQuestions studentQuestions5;
-    List<StudentQuestions>listStudentQuestions;
     List<Boolean> isQuestionAvailable;
-    List<CheckBox> lessonOneCheckBox;
-
+    List<Boolean> listChckbox;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_questions, container, false);
         Log.d(TAG, "onCreateView: I am on question fragment");
         questionOne= view.findViewById(R.id.questionOne);
@@ -61,23 +52,20 @@ public class QuestionsFragment extends Fragment implements View.OnClickListener{
         questionThree= view.findViewById(R.id.questionThree);
         questionFour= view.findViewById(R.id.questionFour);
         questionFive= view.findViewById(R.id.questionFive);
-        lessonOneCheckBox= new ArrayList <>();
-        lessonOneCheckBox.add(questionOne);
-        lessonOneCheckBox.add(questionTwo);
-        lessonOneCheckBox.add(questionThree);
-        lessonOneCheckBox.add(questionFour);
-        lessonOneCheckBox.add(questionFive);
-
-
+        send.setOnClickListener(this);
+        listChckbox= new ArrayList<>();
+        listChckbox.add(true);
+        listChckbox.add(true);
+        listChckbox.add(true);
+        listChckbox.add(true);
+        listChckbox.add(true);
         setUpViews(view);
         levelList= new ArrayList <>();
         database = FirebaseDatabase.getInstance();
         ref = database.getReference();
         listStudents= new ArrayList<>();
         getStudents();
-
         recyclerView.setLayoutManager(new LinearLayoutManager(view.getContext(), LinearLayoutManager.VERTICAL, false));
-
         levelList.add(new Level("Level One", "one", "two", "three", "four", "five"));
         levelList.add(new Level("Level Two", "one", "two", "three", "four", "five"));
         levelList.add(new Level("Level Three", "one", "two", "three", "four", "five"));
@@ -88,52 +76,36 @@ public class QuestionsFragment extends Fragment implements View.OnClickListener{
     }
 
     private void getStudents() {
-
         studentModel= new Student();
         studentQuestions= new StudentQuestions();
         isQuestionAvailable= new ArrayList <>();
-        lista= new HashMap<String, String>();
-        studentQuestions1= new StudentQuestions(questionOne.isChecked(), "passed", "this is q1");
-        studentQuestions2= new StudentQuestions(questionTwo.isChecked(), "failed", "this is q2");
-        studentQuestions3= new StudentQuestions(questionThree.isChecked(), "failed", "this is q3");
-        studentQuestions4= new StudentQuestions(questionFour.isChecked(), "failed", "this is q4");
-        studentQuestions5= new StudentQuestions(questionFive.isChecked(), "passed", "this is q5");
-        listStudentQuestions= new ArrayList<>();
-        listStudentQuestions.add(studentQuestions1);
-        listStudentQuestions.add(studentQuestions2);
-        listStudentQuestions.add(studentQuestions3);
-        listStudentQuestions.add(studentQuestions4);
-        listStudentQuestions.add(studentQuestions5);
 
         ChildEventListener childEventListener= new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Log.d("CURRENTUSER=", "onChildAdded: "+dataSnapshot.getKey());
+                Boolean q1=Boolean.parseBoolean(dataSnapshot.child("lesson1").child("1").child("available").getValue().toString());
+                Boolean q2=Boolean.parseBoolean(dataSnapshot.child("lesson1").child("2").child("available").getValue().toString());
+                Boolean q3=Boolean.parseBoolean(dataSnapshot.child("lesson1").child("3").child("available").getValue().toString());
+                Boolean q4=Boolean.parseBoolean(dataSnapshot.child("lesson1").child("4").child("available").getValue().toString());
+                Boolean q5=Boolean.parseBoolean(dataSnapshot.child("lesson1").child("5").child("available").getValue().toString());
 
-                setUpCheckBoxes(dataSnapshot);
-                Log.d("LISTA===", "child==: "+dataSnapshot.getValue());
-                Log.d("LISTA===", "child==: "+dataSnapshot.getKey());
-                for (int i = 0; i < 5; i++) {
+                listChckbox.set(0,q1);
+                listChckbox.set(1,q2);
+                listChckbox.set(2,q3);
+                listChckbox.set(3, q4);
+                listChckbox.set(4, q5);
 
-                    for (int j = 1; j <6 ; j++) {
-                        ref.child("students").child(dataSnapshot.getKey()).child("lesson1").child((i+1)+"").setValue(listStudentQuestions.get(i));
-                        ref.child("students").child(dataSnapshot.getKey()).child("lesson2").child((i+1)+"").setValue(listStudentQuestions.get(i));
-//                        Log.d("AVAILABLE===", ""+ dataSnapshot.child("lesson1").child((i+1)+"").child("available").getValue());
-
-                    }
-
-                }
-
-
-//                ref.child("students").child(dataSnapshot.getKey()).child("lesson1").child("Lesson1").child("1").setValue(studentQuestions1);
-//                ref.child("students").child(dataSnapshot.getKey()).child("lesson1").child("Lesson2").child("2").setValue(studentQuestions1);
-//                ref.child("students").child(dataSnapshot.getKey()).child("lesson1").child("Lesson3").child("3").setValue(studentQuestions1);
-//                ref.child("students").child(dataSnapshot.getKey()).child("lesson1").child("Lesson4").child("4").setValue(studentQuestions1);
-//                ref.child("students").child(dataSnapshot.getKey()).child("lesson1").child("Lesson5").child("5").setValue(studentQuestions1);
-//
+                questionOne.setChecked(listChckbox.get(0));
+                questionTwo.setChecked(listChckbox.get(1));
+                questionThree.setChecked(listChckbox.get(2));
+                questionFour.setChecked(listChckbox.get(3));
+                questionFive.setChecked(listChckbox.get(4));
             }
 
             @Override
             public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
             }
 
             @Override
@@ -154,46 +126,60 @@ public class QuestionsFragment extends Fragment implements View.OnClickListener{
 
         ref.child("students").addChildEventListener(childEventListener);
 
-
-
     }
 
-    private void setUpCheckBoxes(DataSnapshot dataSnapshot) {
-        //getting lesson's question availability and add it to a list
-        for (int k = 0; k < 5; k++){
-            Log.d("AVAILABLE===", ""+ dataSnapshot.child("lesson1").child((k+1)+"").child("available").getValue().toString());
-            Boolean isAvailable= Boolean.valueOf(dataSnapshot.child("lesson1").child((k+1)+"").child("available").getValue().toString());
-            Log.d("Available2=== ", ""+isAvailable);
-            isQuestionAvailable.add(isAvailable);
-        }
-
-        //run the availability list and set the checkboxes
-        for (int h=0; h < 5; h++) {
-            if (isQuestionAvailable.get(h)) {
-                lessonOneCheckBox.get(h).setChecked(true);
-            } else {
-                lessonOneCheckBox.get(h).setChecked(false);
-            }
-        }
-    }
 
     private void setUpViews(View view) {
         send = view.findViewById(R.id.send_button);
         recyclerView= view.findViewById(R.id.question_recyclerview);
-
-
-        setClickCpature();
     }
 
-    private void setClickCpature() {
-        send.setOnClickListener(this);
-
-    }
 
     @Override
     public void onClick(View v) {
+        ChildEventListener childEventListener= new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                listChckbox.set(0,questionOne.isChecked());
+                listChckbox.set(1,questionTwo.isChecked());
+                listChckbox.set(2,questionThree.isChecked());
+                listChckbox.set(3, questionFour.isChecked());
+                listChckbox.set(4, questionFive.isChecked());
 
-        getStudents();
+                for (int i = 1; i < 6; i++) {
+                    ref.child("students").child(dataSnapshot.getKey()).child("lesson1").child((i)+"").child("available").setValue(listChckbox.get((i-1)));
+                    ref.child("students").child(dataSnapshot.getKey()).child("lesson2").child((i)+"").child("available").setValue(listChckbox.get((i-1)));
 
+                }
+                questionOne.setChecked(listChckbox.get(0));
+                questionTwo.setChecked(listChckbox.get(1));
+                questionThree.setChecked(listChckbox.get(2));
+                questionFour.setChecked(listChckbox.get(3));
+                questionFive.setChecked(listChckbox.get(4));
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+
+        ref.child("students").addChildEventListener(childEventListener);
     }
+
 }
