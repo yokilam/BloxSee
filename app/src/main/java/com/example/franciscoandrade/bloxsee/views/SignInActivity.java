8a71c05a331343443acbe2c941fe80ef740c1943
7sco <xaviandrade14@gmail.com>
2018-03-27 12:09:37 -0400

    fixed bug causing app to crash at onstart

--- app/src/main/java/com/example/franciscoandrade/bloxsee/views/SignInActivity.java
+++ app/src/main/java/com/example/franciscoandrade/bloxsee/views/SignInActivity.java
@@ -27,67 +27,55 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
 
     private Button teacher;
     private Button student;
-    private BottomSheetBehavior mBottomSheetBehavior;
-    private FrameLayout loginContainer;
     TextView exit;
     android.support.v4.app.FragmentManager fragmentManager = getSupportFragmentManager();
     TeacherSignInFragment teacherSignInFragment;
     StudentSignInFragment studentSignInFragment;
     Animation fromBottom, fromTop;
     LottieAnimationView lottieAnimationView;
-    LinearLayout mainContainer, studentLayout, teacherLayout;
+    LinearLayout mainContainer;
     ImageView bloxseeIcon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
-        lottieAnimationView = findViewById(R.id.lottieAnimationView);
+//        lottieAnimationView = findViewById(R.id.lottieAnimationView);
         mainContainer= findViewById(R.id.main_content);
         bloxseeIcon= findViewById(R.id.bloxsee_icon);
-//        studentLayout= findViewById(R.id.student_layout);
-//        teacherLayout= findViewById(R.id.teacher_layout);
         setUpViews();
-        student.setVisibility(View.GONE);
-        teacher.setVisibility(View.GONE);
-        bloxseeIcon.setVisibility(View.GONE);
+
 //        lottieLayout= findViewById(R.id.lottie_layout);
-        lottieAnimationView.addAnimatorListener(new Animator.AnimatorListener() {
-            @Override
-            public void onAnimationStart(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                lottieAnimationView.clearAnimation();
-                lottieAnimationView.setVisibility(View.GONE);
-                bloxseeIcon.setVisibility(View.VISIBLE);
-                mainContainer.setBackgroundResource(R.drawable.bloxseebackground);
-                fromBottom = AnimationUtils.loadAnimation(SignInActivity.this, R.anim.frombottom);
-                fromTop = AnimationUtils.loadAnimation(SignInActivity.this, R.anim.fromtop);
-                setUpButtonAnimation();
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-
-            }
-
-            @Override
-            public void onAnimationRepeat(Animator animation) {
-
-            }
-        });
-
-//        Intent intent = new Intent(this, BlocklyActivity.class);
-//        startActivity(intent);
-
+//        lottieAnimationView.addAnimatorListener(new Animator.AnimatorListener() {
+//            @Override
+//            public void onAnimationStart(Animator animation) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationEnd(Animator animation) {
+//                lottieAnimationView.clearAnimation();
+//                lottieAnimationView.setVisibility(View.GONE);
+//                bloxseeIcon.setVisibility(View.VISIBLE);
+//                mainContainer.setBackgroundResource(R.drawable.bloxseebackground);
+//                fromBottom = AnimationUtils.loadAnimation(SignInActivity.this, R.anim.frombottom);
+//                fromTop = AnimationUtils.loadAnimation(SignInActivity.this, R.anim.fromtop);
+//                setUpButtonAnimation();
+//            }
+//
+//            @Override
+//            public void onAnimationCancel(Animator animation) {
+//
+//            }
+//
+//            @Override
+//            public void onAnimationRepeat(Animator animation) {
+//
+//            }
+//        });
 
         teacher.setOnClickListener(this);
         student.setOnClickListener(this);
-
-
         teacherSignInFragment= new TeacherSignInFragment();
         studentSignInFragment= new StudentSignInFragment();
         exit.setOnClickListener(new View.OnClickListener() {
@@ -117,37 +105,15 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
 
     @SuppressLint("ClickableViewAccessibility")
     public void setUpViews() {
-        //View bottomSheet = findViewById(R.id.bottom_sheet);
         teacher = findViewById(R.id.teacherBtn);
         student = findViewById(R.id.studentBtn);
         exit = findViewById(R.id.exit);
-       // mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
-        //setBottomSheetBehavior();
     }
 
-    private void setBottomSheetBehavior() {
-        mBottomSheetBehavior.setPeekHeight(300);
-        mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
-        mBottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
-            @Override
-            public void onStateChanged(View bottomSheet, int newState) {
-                if (newState == BottomSheetBehavior.STATE_HIDDEN) {
-                    mBottomSheetBehavior.setPeekHeight(70);
-                }
-            }
-
-            @Override
-            public void onSlide(View bottomSheet, float slideOffset) {
-            }
-        });
-    }
 
     @Override
     public void onClick(View v) {
-//        mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
-
         switch (v.getId()) {
-            // TODO: Fragments are being added to stack, make fragments not to be added to stack
             case R.id.teacherBtn: {
                 exit.setBackgroundColor(Color.parseColor("#FF6699"));
                 exit.setVisibility(View.VISIBLE);
