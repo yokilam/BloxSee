8a9dba33fc0a05e9f2edae900707726b9ed74325
Joanne Song <joannesong@ac.c4q.nyc>
2018-03-31 00:05:46 -0400

    Lessons in Progress Frag expands and collapses correctly. Added the teacher b/g to Progress Frag

--- app/src/main/java/com/example/franciscoandrade/bloxsee/views/teacher/TeacherProgressViewHolder.java
+++ app/src/main/java/com/example/franciscoandrade/bloxsee/views/teacher/TeacherProgressViewHolder.java
@@ -1,7 +1,6 @@
 package com.example.franciscoandrade.bloxsee.views.teacher;
 
 import android.content.Context;
-import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.util.SparseBooleanArray;
@@ -28,8 +27,8 @@ public class TeacherProgressViewHolder extends RecyclerView.ViewHolder implement
         super(itemView);
 
         name = itemView.findViewById(R.id.name_progress);
-        lessonOne = itemView.findViewById(R.id.lesson_one);
-        questionOne= itemView.findViewById(R.id.lesson_1);
+        lessonOne = itemView.findViewById(R.id.lessons);
+        questionOne= itemView.findViewById(R.id.lesson_one);
         questionTwo= itemView.findViewById(R.id.lesson_two);
         questionThree= itemView.findViewById(R.id.lesson_three);
         questionFour= itemView.findViewById(R.id.lesson_four);
@@ -59,7 +58,7 @@ public class TeacherProgressViewHolder extends RecyclerView.ViewHolder implement
     public void onClick(View v) {
 
         switch(v.getId()){
-            case R.id.lesson_one:
+            case R.id.lessons:
                 expandableLayoutAnimation.changeExpandableLayoutColorAndAnimation
                         (v, expandableLayout, R.color.material_orange_200, expandState);
                 changeColor(progress1.getLesson1().get(0).getState(), questionOne);
@@ -72,8 +71,12 @@ public class TeacherProgressViewHolder extends RecyclerView.ViewHolder implement
                 Log.d("COLORS1==", "onClick: "+progress1.getLesson1().get(2));
                 Log.d("COLORS1==", "onClick: "+progress1.getLesson1().get(3));
                 Log.d("COLORS1==", "onClick: "+progress1.getLesson1().get(4));
+
+                if(expandableLinearLayoutQuestions.isExpanded()){
+                    expandableLinearLayoutQuestions.collapse();
+                }
                 break;
-            case R.id.lesson_1:
+            case R.id.lesson_one:
                 expandableLayoutAnimation.changeExpandableLayoutColorAndAnimation(v, expandableLinearLayoutQuestions, R.color.material_blue_100, expandState);
                 break;
             case R.id.lesson_two:
