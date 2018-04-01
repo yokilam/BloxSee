20f77a2f9b5d24936620d4bb66569ebd4b61de3d
7sco <xaviandrade14@gmail.com>
2018-03-31 23:24:28 -0400

    student average colors being displayed

--- app/src/main/java/com/example/franciscoandrade/bloxsee/views/teacher/TeacherProgressViewHolder.java
+++ app/src/main/java/com/example/franciscoandrade/bloxsee/views/teacher/TeacherProgressViewHolder.java
@@ -16,6 +16,9 @@ import com.example.franciscoandrade.bloxsee.model.Progress;
 import com.example.franciscoandrade.bloxsee.util.ExpandableLayoutAnimation;
 import com.github.aakira.expandablelayout.ExpandableLinearLayout;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TeacherProgressViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
     private TextView name;
     private TextView questionOne, questionTwo, questionThree, questionFour, questionFive;
@@ -28,6 +31,9 @@ public class TeacherProgressViewHolder extends RecyclerView.ViewHolder implement
 
     CardView cardLessons;
     LinearLayout questionsView;
+    CardView name_Student_Card;
+
+    int average=100;
 
     public TeacherProgressViewHolder(View itemView) {
         super(itemView);
@@ -42,6 +48,7 @@ public class TeacherProgressViewHolder extends RecyclerView.ViewHolder implement
         q3= itemView.findViewById(R.id.q3);
         q4= itemView.findViewById(R.id.q4);
         q5= itemView.findViewById(R.id.q5);
+        name_Student_Card= itemView.findViewById(R.id.name_Student_Card );
 
 
         cardLessons= itemView.findViewById(R.id.cardLessons);
@@ -61,6 +68,65 @@ public class TeacherProgressViewHolder extends RecyclerView.ViewHolder implement
         name.setText(progress.getName());
         progress1= progress;
         this.context=context;
+
+        calculateAVerage();
+
+    }
+
+    private void calculateAVerage() {
+        List<String> lesson1= new ArrayList<>();
+        List<String> lesson2= new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            lesson1.add(progress1.getLesson1().get(i).getState());
+
+
+            lesson2.add(progress1.getLesson1().get(i).getState());
+        }
+
+
+        for (int i = 0; i < 5; i++) {
+            Log.d("AVERAGE=", "calculateAVerage: "+progress1.getName()+" - "+lesson1.get(i));
+            Log.d("AVERAGE=", "calculateAVerage: "+progress1.getName()+" - "+lesson2.get(i));
+            if(lesson1.get(i).equals("passed")){
+
+                average=average;
+            }
+
+            if (lesson2.get(i).equals("passed")){
+                average=average;
+            }
+
+            if(lesson1.get(i).equals("failed") || lesson2.get(i).equals("failed") || lesson1.get(i).equals("fail")|| lesson2.get(i).equals("fail")){
+                average-=10;
+            }
+
+
+        }
+
+            Log.d("AVERAGE=", "calculateAVerage: "+progress1.getName()+" - "+average);
+
+
+        setColorAverage();
+
+    }
+
+    private void setColorAverage() {
+
+        if(average>=80){
+            name_Student_Card.setCardBackgroundColor(Color.parseColor("#26A69A"));
+        }
+
+
+        if(average>=60 && average<80){
+            name_Student_Card.setCardBackgroundColor(Color.parseColor("#ffc910"));
+        }
+
+        if(average<59 ){
+            name_Student_Card.setCardBackgroundColor(Color.parseColor("#FF3333"));
+        }
+
+
 
     }
 
